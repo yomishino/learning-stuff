0eccc39abd3bc2543a05f4c5abc9685e005d8677
yomishino <yomishino@users.noreply.github.com>
2017-08-08 23:29:03 +1000

    Add my solution to ch03ex09

---
+++ haskell/real-world-haskell/ch03ex09/Tree.hs
@@ -0,0 +1,17 @@
+{-
+    Author: yomishino
+
+    Chapter 3, Exercise 9 (end of chap.)
+    Write a function that will determine the height of a tree.
+    
+    The definition of the binary tree type is given in the book. 
+-}
+
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+            deriving (Show)
+
+-- |Computes the height of the tree.
+height :: Tree a -> Int
+height Empty = 0
+height (Node x l r) = 1 + (max (height l) (height r))
