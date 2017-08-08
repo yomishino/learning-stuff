2968ad32548e4803ed7c546846f9f105554cc7bc
yomishino <yomishino@users.noreply.github.com>
2017-08-08 14:58:53 +1000

    Add my solution to ch03ex01

---
+++ haskell/real-world-haskell/ch03ex01/ListEx.hs
@@ -0,0 +1,17 @@
+{-
+    Author: yomishino
+
+    Chapter 3, Exercise 1
+    Write a function that takes a List a and generates a [a].
+-}
+
+-- Definition of List a is given in the book.
+data List a = Cons a (List a)
+            | Nil
+            deriving (Show)
+
+
+-- | Generates a [a] from a List a.
+toList :: List a -> [a]
+toList Nil = []
+toList (Cons a as) = a:(toList as)
